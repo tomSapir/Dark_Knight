d3b92afa51b95b4317bf141a4304fff06d797340
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-09-17 20:25:38 +0300

    player can walk and run

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private static readonly string sr_AnimatorCrouchingParameterName = "IsCrouching";
+    private static readonly string sr_AnimatorWalkingParameterName = "IsWalking";
+    private static readonly string sr_AnimatorRunningParameterName = "IsRunning";
+    private static readonly string sr_AnimatorJumpingParameterName = "IsJumping";
+
     public CharacterController2D m_Controller;
 
-    public float m_RunSpeed = 30f;
-
+    public float m_RunSpeed = 20f;
     private float m_HorizontalMove = 0f;
     private bool  m_Jump = false;
     private bool  m_Crouch = false;
@@ -19,8 +23,10 @@ public class PlayerMovement : MonoBehaviour
         m_HorizontalMove = Input.GetAxisRaw("Horizontal") * m_RunSpeed;
         m_Animator.SetFloat("Speed", Mathf.Abs(m_HorizontalMove));
 
-        checkIfNeedToJump();
-        checkIfNeedToCrouch();
+        checkIfWalking();
+        checkIfRunning();
+        checkIfJumping();
+        checkIfCrouching();
     }
 
     private void FixedUpdate()
@@ -29,16 +35,38 @@ public class PlayerMovement : MonoBehaviour
         m_Jump = false;
     }
 
-    private void checkIfNeedToJump()
+    private void checkIfWalking()
+    {
+        bool isWalking = !Input.GetKey(KeyCode.LeftShift) && m_HorizontalMove != 0 && !m_Animator.GetBool(sr_AnimatorJumpingParameterName);
+
+        m_Animator.SetBool(sr_AnimatorWalkingParameterName, isWalking);
+    }
+
+    private void checkIfRunning()
+    {
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && m_HorizontalMove != 0 && !m_Animator.GetBool(sr_AnimatorJumpingParameterName);
+
+        m_Animator.SetBool(sr_AnimatorRunningParameterName, isRunning);
+        if (isRunning)
+        {
+            m_RunSpeed = 40f;
+        }
+        else
+        {
+            m_RunSpeed = 20f;
+        }
+    }
+
+    private void checkIfJumping()
     {
         if (Input.GetButtonDown("Jump"))
         {
             m_Jump = true;
-            m_Animator.SetBool("IsJumping", true);
+            m_Animator.SetBool(sr_AnimatorJumpingParameterName, true);
         }
     }
 
-    private void checkIfNeedToCrouch()
+    private void checkIfCrouching()
     {
         if (Input.GetButtonDown("Crouch"))
         {
@@ -52,11 +80,11 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnPlayerLanding()
     {
-        m_Animator.SetBool("IsJumping", false);
+        m_Animator.SetBool(sr_AnimatorJumpingParameterName, false);
     }
 
     public void OnPlayerCrouching(bool i_IsCrouching)
     {
-        m_Animator.SetBool("IsCrouching", i_IsCrouching);
+        m_Animator.SetBool(sr_AnimatorCrouchingParameterName, i_IsCrouching);
     }
 }
