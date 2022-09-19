ecbe93af7430b07f8497a89ea838bd3ad130648f
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-09-19 14:03:02 +0300

    player can crouch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,47 +24,85 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // handle speed if holding shift
-        if(Input.GetKey(KeyCode.LeftShift))
+        handleSpeed();
+        handleMoveSideways();
+        handleDirectionChange();
+        checkIfOnTheGround();
+        handleJumping();
+        updateAnimationsParameters();
+    }
+
+    private void handleSpeed()
+    {
+        if (m_Animator.GetBool("IsCrouching"))
         {
-            m_Animator.SetBool("IsRunning", true);
-            m_MoveSpeed = 15f;
+            m_MoveSpeed = 0f;
         }
         else
         {
-            m_Animator.SetBool("IsRunning", false);
-            m_MoveSpeed = 8f;
+            if (Input.GetKey(KeyCode.LeftShift)) // if running
+            {
+                m_MoveSpeed = 15f;
+            }
+            else
+            {
+                m_MoveSpeed = 8f;
+            }
         }
+    }
 
-
-        // move sideways
+    private void handleMoveSideways()
+    {
         m_RigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * m_MoveSpeed, m_RigidBody.velocity.y);
+    }
 
-        // handle directions change
-        if(m_RigidBody.velocity.x < 0)
+    private void handleDirectionChange()
+    {
+        if (m_RigidBody.velocity.x < 0)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        else if(m_RigidBody.velocity.x > 0)
+        else if (m_RigidBody.velocity.x > 0)
         {
             transform.localScale = Vector3.one;
         }
+    }
 
-    
-        // checking if on the ground
+    private void checkIfOnTheGround()
+    {
         m_IsOnGround = Physics2D.OverlapCircle(m_GroundCheckPoint.position, .2f, m_WhatIsGround);
+    }
 
-
-
-        // jumping
-        if(Input.GetButtonDown("Jump") && m_IsOnGround)
+    private void handleJumping()
+    {
+        if (Input.GetButtonDown("Jump") && m_IsOnGround)
         {
             m_RigidBody.velocity = new Vector2(m_RigidBody.velocity.x, m_JumpForce);
         }
+    }
 
-
-        // update animation parameters
+    private void updateAnimationsParameters()
+    {
         m_Animator.SetBool("IsOnGround", m_IsOnGround);
         m_Animator.SetFloat("Speed", Mathf.Abs(m_RigidBody.velocity.x));
+
+        if(m_MoveSpeed == 15f)
+        {
+            m_Animator.SetBool("IsRunning", true);
+        }
+        else 
+        {
+            m_Animator.SetBool("IsRunning", false);
+        }
+
+
+        if((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && m_IsOnGround)
+        {
+            m_Animator.SetBool("IsCrouching", true);
+        }
+        else
+        {
+            m_Animator.SetBool("IsCrouching", false);
+        }
     }
 }
