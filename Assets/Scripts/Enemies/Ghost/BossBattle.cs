d60c570f8fc7eb5f3365d7aec958c6ee1a213c1e
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-09-30 14:44:47 +0300

    .

--- Assets/Scripts/Enemies/Ghost/BossBattle.cs
+++ Assets/Scripts/Enemies/Ghost/BossBattle.cs
@@ -18,67 +18,44 @@ public class BossBattle : MonoBehaviour
     public Animator m_Animator;
     public Transform m_Boss;
 
+    [SerializeField] private float m_TimeBetweenShots1, m_TimeBetweenShots2;
+    private float m_ShotCounter;
+    [SerializeField] private GameObject m_Bullet;
+    public Transform m_ShotPoint;
+
+    public GameObject m_WinObjects;
+    private bool m_BattleEnded;
+
     void Start()
     {
         m_Camera = FindObjectOfType<CameraController>();
         m_Camera.enabled = false;
-
         m_ActiveCounter = m_ActiveTime;
+        m_ShotCounter = m_TimeBetweenShots1;
     }
 
     void Update()
     {
         m_Camera.transform.position = Vector3.MoveTowards(m_Camera.transform.position, m_CameraPosition.position, m_CameraSpeed * Time.deltaTime);
 
-        if(BossHealthController.m_Instance.m_CurrentHealth > m_TreshHold1) // in phase 1
+        if (!m_BattleEnded)
         {
-            if(m_ActiveCounter > 0)
+            if (BossHealthController.m_Instance.m_CurrentHealth > m_TreshHold1) // in phase 1
             {
-                m_ActiveCounter -= Time.deltaTime;
-                if(m_ActiveCounter <= 0)
+                if (m_ActiveCounter > 0)
                 {
-                    m_FadoutCounter = m_FadeoutTime;
-                    m_Animator.SetTrigger("Vanish");
-                }
-            }
-            else if(m_FadoutCounter > 0)
-            {
-                m_FadoutCounter -= Time.deltaTime;
-                if(m_FadoutCounter <= 0)
-                {
-                    m_Boss.gameObject.SetActive(false);
-                    m_InactiveCounter = m_InactiveTime;
-                }
-            }
-            else if(m_InactiveCounter > 0)
-            {
-                m_InactiveCounter -= Time.deltaTime;
-                if(m_InactiveCounter <= 0)
-                {
-                    m_Boss.position = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)].position;
-                    m_Boss.gameObject.SetActive(true);
-                    m_ActiveCounter = m_ActiveTime;
-                }
-            }
-        }
-        else
-        {
-            if(m_TargetPoint == null)
-            {
-                m_TargetPoint = m_Boss;
-                m_FadoutCounter = m_FadeoutTime;
-                m_Animator.SetTrigger("Vanish");
-            }
-            else
-            {
-                if (Vector3.Distance(m_Boss.position, m_TargetPoint.position) > .02f)
-                {
-                    m_Boss.position = Vector3.MoveTowards(m_Boss.position, m_TargetPoint.position, m_MoveSpeed * Time.deltaTime);
-
-                    if (Vector3.Distance(m_Boss.position, m_TargetPoint.position) <= .02f)
+                    m_ActiveCounter -= Time.deltaTime;
+                    if (m_ActiveCounter <= 0)
                     {
                         m_FadoutCounter = m_FadeoutTime;
                         m_Animator.SetTrigger("Vanish");
+                    }
+
+                    m_ShotCounter -= Time.deltaTime;
+                    if (m_ShotCounter <= 0)
+                    {
+                        m_ShotCounter = m_TimeBetweenShots1;
+                        Instantiate(m_Bullet, m_ShotPoint.position, Quaternion.identity);
                     }
                 }
                 else if (m_FadoutCounter > 0)
@@ -96,26 +73,119 @@ public class BossBattle : MonoBehaviour
                     if (m_InactiveCounter <= 0)
                     {
                         m_Boss.position = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)].position;
+                        m_Boss.gameObject.SetActive(true);
+                        m_ActiveCounter = m_ActiveTime;
+                    }
 
-                        m_TargetPoint = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)];
+                    m_ShotCounter = m_TimeBetweenShots1;
+                }
+            }
+            else
+            {
+                if (m_TargetPoint == null)
+                {
+                    m_TargetPoint = m_Boss;
+                    m_FadoutCounter = m_FadeoutTime;
+                    m_Animator.SetTrigger("Vanish");
+                }
+                else
+                {
+                    if (Vector3.Distance(m_Boss.position, m_TargetPoint.position) > .02f)
+                    {
+                        m_Boss.position = Vector3.MoveTowards(m_Boss.position, m_TargetPoint.position, m_MoveSpeed * Time.deltaTime);
 
-                        int whileBreaker = 0;
-                        while(m_TargetPoint.position == m_Boss.position && whileBreaker < 100)
+                        if (Vector3.Distance(m_Boss.position, m_TargetPoint.position) <= .02f)
                         {
-                            m_TargetPoint = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)];
-                            whileBreaker++;
+                            m_FadoutCounter = m_FadeoutTime;
+                            m_Animator.SetTrigger("Vanish");
                         }
 
-                        m_Boss.gameObject.SetActive(true);
-                        
+                        m_ShotCounter -= Time.deltaTime;
+                        if (m_ShotCounter <= 0)
+                        {
+                            if (BossHealthController.m_Instance.m_CurrentHealth > m_TreshHold2)
+                            {
+                                m_ShotCounter = m_TimeBetweenShots1;
+                            }
+                            else
+                            {
+                                m_ShotCounter = m_TimeBetweenShots2;
+                            }
+
+                            Instantiate(m_Bullet, m_ShotPoint.position, Quaternion.identity);
+                        }
+                    }
+                    else if (m_FadoutCounter > 0)
+                    {
+                        m_FadoutCounter -= Time.deltaTime;
+                        if (m_FadoutCounter <= 0)
+                        {
+                            m_Boss.gameObject.SetActive(false);
+                            m_InactiveCounter = m_InactiveTime;
+                        }
+                    }
+                    else if (m_InactiveCounter > 0)
+                    {
+                        m_InactiveCounter -= Time.deltaTime;
+                        if (m_InactiveCounter <= 0)
+                        {
+                            m_Boss.position = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)].position;
+
+                            m_TargetPoint = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)];
+
+                            int whileBreaker = 0;
+                            while (m_TargetPoint.position == m_Boss.position && whileBreaker < 100)
+                            {
+                                m_TargetPoint = m_SpawnPoints[Random.Range(0, m_SpawnPoints.Length)];
+                                whileBreaker++;
+                            }
+
+                            m_Boss.gameObject.SetActive(true);
+
+                            if (BossHealthController.m_Instance.m_CurrentHealth > m_TreshHold2)
+                            {
+                                m_ShotCounter = m_TimeBetweenShots1;
+                            }
+                            else
+                            {
+                                m_ShotCounter = m_TimeBetweenShots2;
+                            }
+                        }
                     }
                 }
+            }
+        }
+        else
+        {
+            m_FadoutCounter -= Time.deltaTime;
+            if(m_FadoutCounter < 0)
+            {
+                if(m_WinObjects != null)
+                {
+                    m_WinObjects.SetActive(true);
+                    m_WinObjects.transform.SetParent(null);
+                }
+
+                m_Camera.enabled = true;
+                gameObject.SetActive(false);
             }
         }
     }
 
     public void EndBattle()
     {
-        gameObject.SetActive(false);
+        m_BattleEnded = true;
+        m_FadoutCounter = m_FadeoutTime;
+        m_Animator.SetTrigger("Vanish");
+        m_Boss.GetComponent<Collider2D>().enabled = false;
+
+        BossBullet[] bulletsOnScene = FindObjectsOfType<BossBullet>();
+        if(bulletsOnScene.Length > 0)
+        {
+            foreach(BossBullet bullet in bulletsOnScene)
+            {
+                Destroy(bullet.gameObject);
+            }
+        }
     }
 }
