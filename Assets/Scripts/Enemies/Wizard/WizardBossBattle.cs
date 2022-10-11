080503772adb1463737a6f3a42baabf742583293
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-10-11 16:05:07 +0300

    z

--- Assets/Scripts/Enemies/Wizard/WizardBossBattle.cs
+++ Assets/Scripts/Enemies/Wizard/WizardBossBattle.cs
@@ -28,6 +28,8 @@ public class WizardBossBattle : MonoBehaviour
     private int m_AttackCounter;
     private int m_AttackIndex = 1;
 
+    [SerializeField] private GameObject m_WinObject;
+
     void Start()
     {
         m_Player = GameObject.Find("Player").transform;
@@ -41,57 +43,54 @@ public class WizardBossBattle : MonoBehaviour
     {
         m_Camera.transform.position = Vector3.MoveTowards(m_Camera.transform.position, m_CameraPosition.position, m_CameraMovementSpeed * Time.deltaTime);
 
-        if(WizardBossHealthController.m_Instance.m_CurrentHealth > m_Treshold1) // Phase 1
+        switch (m_State)
         {
-            switch(m_State)
-            {
-                case eWizardState.Cooldown:
+            case eWizardState.Cooldown:
+                {
+                    m_CooldownCounter -= Time.deltaTime;
+                    if (m_CooldownCounter <= 0)
                     {
-                        m_CooldownCounter -= Time.deltaTime;
-                        if(m_CooldownCounter <= 0)
-                        {
-                            findNextTarget();
-                            flipIfNeed();
-                            m_State = eWizardState.Walking;
-                        }
-
-                        break;
+                        findNextTarget();
+                        flipIfNeed();
+                        m_State = eWizardState.Walking;
                     }
-                case eWizardState.Walking:
+
+                    break;
+                }
+            case eWizardState.Walking:
+                {
+                    m_Animator.SetBool("Walk", true);
+                    m_TheBoss.position = Vector2.MoveTowards(m_TheBoss.position, m_TargetPoint, m_MoveSpeed * Time.deltaTime);
+                    if (Mathf.Abs(m_TheBoss.position.x - m_TargetPoint.x) < .1f)
                     {
-                        m_Animator.SetBool("Walk", true);
-                        m_TheBoss.position = Vector2.MoveTowards(m_TheBoss.position, m_TargetPoint, m_MoveSpeed * Time.deltaTime);
-                        if (Mathf.Abs(m_TheBoss.position.x - m_TargetPoint.x) < .1f)
-                        {
-                            m_Animator.SetBool("Walk", false);
-                            m_State = eWizardState.Attacking;
-                            m_AttackCounter = m_AmountOfAttacks;
-                        }
-
-                        break;
+                        m_Animator.SetBool("Walk", false);
+                        m_State = eWizardState.Attacking;
+                        m_AttackCounter = m_AmountOfAttacks;
                     }
-                case eWizardState.Attacking:
+
+                    break;
+                }
+            case eWizardState.Attacking:
+                {
+                    if (m_AttackCounter > 0)
                     {
-                        if(m_AttackCounter > 0)
+                        m_AttackCooldownCounter -= Time.deltaTime;
+                        if (m_AttackCooldownCounter <= 0)
                         {
-                            m_AttackCooldownCounter -= Time.deltaTime;
-                            if (m_AttackCooldownCounter <= 0)
-                            {
-                                m_AttackCounter--;
-                                m_Animator.SetTrigger("Attack");
-                                ChangeAttackIndex();
-                                m_AttackCooldownCounter = m_AttackCooldownTime;
-                            }
-                        }
-                        else
-                        {
-                            m_State = eWizardState.Cooldown;
-                            m_CooldownCounter = m_CooldownTime;
-                        }
-
-                        break;
+                            m_AttackCounter--;
+                            m_Animator.SetTrigger("Attack");
+                            ChangeAttackIndex();
+                            m_AttackCooldownCounter = m_AttackCooldownTime;
+                         }
                     }
-            }
+                    else
+                    {
+                         m_State = eWizardState.Cooldown;
+                         m_CooldownCounter = m_CooldownTime;
+                    }
+
+                    break;
+                }
         }
     }
 
@@ -135,6 +134,10 @@ public class WizardBossBattle : MonoBehaviour
 
     public void EndBattle()
     {
+        m_Camera.enabled = true;
+        m_WinObject.SetActive(true);
         gameObject.SetActive(false);
+        AudioManager.m_Instance.PlaySFX(0);
+        AudioManager.m_Instance.PlayLevelMusic();
     }
 }
