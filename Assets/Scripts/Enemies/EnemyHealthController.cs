eceedd8a57cf88aecdd9c19aff0fc50640da7309
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-10-15 19:42:10 +0300

    z

--- Assets/Scripts/Enemies/EnemyHealthController.cs
+++ Assets/Scripts/Enemies/EnemyHealthController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealthController : MonoBehaviour
 {
@@ -8,10 +9,12 @@ public class EnemyHealthController : MonoBehaviour
     [SerializeField] private int m_MaxHealth = 100;
     public int m_CurrentHealth;
     [SerializeField] private GameObject m_BloodEffect;
+    [SerializeField] private Slider m_HealthSlider;
 
     void Start()
     {
         m_CurrentHealth = m_MaxHealth;
+        UpdateHealth(m_CurrentHealth);
     }
 
     public void DamageEnemy(int i_DamageAmount)
@@ -19,6 +22,7 @@ public class EnemyHealthController : MonoBehaviour
         if(m_CurrentHealth > 0)
         {
             m_CurrentHealth -= i_DamageAmount;
+            UpdateHealth(m_CurrentHealth);
             Instantiate(m_BloodEffect, transform.position, transform.rotation);
 
             try
@@ -44,8 +48,15 @@ public class EnemyHealthController : MonoBehaviour
 
         if (gameObject.name == "Flying_Eye_Enemy")
         {
+            Destroy(m_HealthSlider);
             Destroy(gameObject);
         }
+    }
+
+    public void UpdateHealth(int m_CurrentHealth)
+    {
+        m_HealthSlider.maxValue = m_MaxHealth;
+        m_HealthSlider.value = m_CurrentHealth;
     }
 }
 
