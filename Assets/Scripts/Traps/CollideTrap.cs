394186ae83421b02045f7471bef58acc96c21688
tomSapir <70592152+tomSapir@users.noreply.github.com>
2022-10-20 15:53:56 +0300

    z

---
+++ Assets/Scripts/Traps/CollideTrap.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CollideTrap : MonoBehaviour
+{
+    [SerializeField] private int m_DamageAmount;
+    [SerializeField] private GameObject m_ImpactEffect;
+
+    private void OnCollisionEnter2D(Collision2D i_Other)
+    {
+        if (i_Other.gameObject.tag == "Player")
+        {
+            PlayerHealthController.m_Instance.TakeDamage(m_DamageAmount);
+        }
+
+        if (m_ImpactEffect != null)
+        {
+            Instantiate(m_ImpactEffect, transform.position, transform.rotation);
+        }
+    }
+}
